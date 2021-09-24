9eb6b34c6ad84c7d1fceb6231535680dd8c97525
Rennlad <rennickman@gmail.com>
2021-09-25 00:25:37 +0100

    navbar done - starting menu

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
+
+import { Nav, NavbarContainer, NavLogo, NavIcon, MobileIcon } from './NavbarElements';
+
+
+
+
+const Navbar = () => {
+
+    const [ click, setClick ] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
+    return (
+        <>
+            <IconContext.Provider value={{ color: '#fff' }}>
+                <Nav>
+                    <NavbarContainer>
+                        <NavLogo to="/">
+                            <NavIcon />
+                            ULTRA
+                        </NavLogo>
+
+                        <MobileIcon onClick={handleClick}>
+                            {click ? <FaTimes /> : <FaBars />}
+                        </MobileIcon>
+                    </NavbarContainer>
+                </Nav>
+            </IconContext.Provider>
+        </>
+    );
+};
+
+export default Navbar;
